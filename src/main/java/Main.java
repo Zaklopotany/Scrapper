e98b5a2485e60ba59ec79c8448b5a5a8eedbd491
michal <marry.jane.kelly@gmail.com>
2018-10-14 22:34:40 +0200

    Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,14 @@
-
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.WebClient;
 
 public class Main {
 
     public static void main(String[] args) {
+        //webClient settings
+        WebClient webClient = new WebClient(BrowserVersion.CHROME);
+
+        webClient.getOptions().setJavaScriptEnabled(true);
+
 
     }
 }
