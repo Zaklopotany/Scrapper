570183a5a4e88228728e69a2618d8cab44d6d4bb
michal <marry.jane.kelly@gmail.com>
2018-10-16 07:45:40 +0200

    Scrap tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,16 @@
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlListItem;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
+import java.util.List;
 import java.util.logging.Level;
 
 public class Main {
 
-    public static String MAIN_URL = "https://www.wp.pl";
+    public static String MAIN_URL = "";
 
     public static void main(String[] args) {
         //webClient settings
@@ -23,9 +27,25 @@ public class Main {
         webClient.waitForBackgroundJavaScript(300000);
 
         try {
-            HtmlPage page =  webClient.getPage(MAIN_URL);
-            System.out.println(page.asXml());
+            HtmlPage page = webClient.getPage(MAIN_URL);
+            System.out.println("Main page not emtpy " + page != null );
+            HtmlElement htmlElement =  page.getFirstByXPath("//li[contains(@class, 'page-next')]");
+
+            List<HtmlAnchor> htmlAnchor = htmlElement.getByXPath("//a");
+            HtmlAnchor htmlAnchor1 = (HtmlAnchor) htmlElement.getFirstChild();
+            System.out.println(htmlElement);
+            System.out.println(htmlAnchor1);
+
+
+            HtmlPage page1 = htmlAnchor1.click();
+
+            System.out.println(page.getUrl());
+            System.out.println(page1.getUrl());
+
+
+
 //
+
         } catch (Exception e) {
             e.printStackTrace();
 
---
+++ src/main/java/ScrapResources.java
@@ -0,0 +1,2 @@
+public abstract class ScrapResources {
+}
